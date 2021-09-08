437743a40e97e4e08a008b4e1bb1c7faeafa98ae
imonaar <kevinmonari22@gmail.com>
2021-09-08 17:28:58 +0300

    email validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import "./styles/styles.scss";
 
 console.log("hello world!");
+
+const form = document.forms['form']['email']
+
+const validateEmail = (email) =>{
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
