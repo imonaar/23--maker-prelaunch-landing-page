b5777156d26cd911817514df79fb3ee0a6f910f7
imonaar <kevinmonari22@gmail.com>
2021-09-08 21:17:32 +0300

    email validation

--- src/index.js
+++ src/index.js
@@ -2,10 +2,41 @@ import "./styles/styles.scss";
 
 console.log("hello world!");
 
-const form = document.forms['form']['email']
+const form = document.forms['form']
+const email = document.forms['form']['email']
+const error = document.querySelector('span.error')
 
-const validateEmail = (email) =>{
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+email.addEventListener("input", (event) => {
+  if(email.validity.valid){
+    
+    error.textContent = ""
+
+  } else{
+    showError()
+  }
+});
+
+form.addEventListener('submit', (event)=>{
+  if(!email.validity.valid){
+    showError()
+
+    event.preventDefault()
+  }
+})
+
+function showError(){
+  if(email.validity.valueMissing){
+
+    error.textContent = 'You need to enter an e-mail address.';
+
+  } else if(email.validity.typeMismatch){
+
+    error.textContent ="Oops! That doesn’t look like an email address"
+
+  } else if(email.validity.tooShort){
+
+    error.textContent =`Email should be at least ${ email.minLength } characters; you entered ${ email.value.length }.`;
+
+  }
 }
 
